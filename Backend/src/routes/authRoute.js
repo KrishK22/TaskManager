0be675e7b93ac60f1a7d1965e004a48fe7500540
Krish Kumar <krish.k2024@gmail.com>
2025-04-28 02:03:32 +0530

    authRoute

--- Backend/src/routes/authRoute.js
+++ Backend/src/routes/authRoute.js
@@ -1,5 +1,14 @@
 import express from 'express'
+import { signup, login, logout, checkAuth } from '../controllers/authController.js'
+import { protectedRoute } from '../middleware/protectedRoute.js'
+
 
 const router = express.Router()
 
 router.post('/signup', signup)
+router.post('/login', login)
+router.post('/logout', logout)
+router.get('/get', protectedRoute, checkAuth)
+
+
+export default router
